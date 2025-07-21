9174ff077ec81409a4998ce0346c02b7d581a525
Aoi Karasu <aoikarasu@gmail.com>
2025-07-21 11:55:22 +0200

    Show all local IPv4s with interface and mask, print neofetch- style color bars

--- index.js
+++ index.js
@@ -34,11 +34,81 @@ function getPackages() {
   }
 }
 
+// Returns array of local IPs in form: '{name, address, maskBits}'
+function getLocalIPs() {
+  const ips = [];
+  const nets = os.networkInterfaces();
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      if (net.family === 'IPv4' && !net.internal) {
+        // Calculate CIDR prefix (netmask)
+        let mask = net.netmask;
+        let maskBits = 0;
+        if (mask && typeof mask === 'string') {
+          maskBits = mask.split('.').map(Number)
+            .map(octet => octet.toString(2))
+            .join('').split('1').length - 1;
+        }
+        ips.push({
+          name,
+          address: net.address,
+          maskBits,
+        });
+      }
+    }
+  }
+  return ips;
+}
+
+function getLocale() {
+  return Intl.DateTimeFormat().resolvedOptions().locale;
+}
+
+function getBattery() {
+  try {
+    if (process.platform === 'linux') {
+      const res = execSync("upower -i $(upower -e | grep BAT) | grep percentage | awk '{print $2}'", { encoding: 'utf-8' }).trim();
+      if (res) return res;
+    } else if (process.platform === 'darwin') {
+      const res = execSync("pmset -g batt | grep -Eo '[0-9]+%' | head -1", { encoding: 'utf-8' }).trim();
+      if (res) return res;
+    } else if (process.platform === 'win32') {
+      const res = execSync("WMIC PATH Win32_Battery Get EstimatedChargeRemaining", { encoding: 'utf-8' }).split('\n')[1];
+      if(res && res.trim()) return res.trim() + '%';
+    }
+  } catch {}
+  return 'unknown';
+}
+
+function printColorBars() {
+  const colors = [
+    chalk.bgBlack('   '),
+    chalk.bgRed('   '),
+    chalk.bgGreen('   '),
+    chalk.bgYellow('   '),
+    chalk.bgBlue('   '),
+    chalk.bgMagenta('   '),
+    chalk.bgCyan('   '),
+    chalk.bgWhite('   ')
+  ];
+  const bright = [
+    chalk.bgGray('   '),
+    chalk.bgRedBright('   '),
+    chalk.bgGreenBright('   '),
+    chalk.bgYellowBright('   '),
+    chalk.bgBlueBright('   '),
+    chalk.bgMagentaBright('   '),
+    chalk.bgCyanBright('   '),
+    chalk.bgWhiteBright('   ')
+  ];
+  console.log('\n' + colors.join('') + '\n' + bright.join('') + '\n');
+}
+
 console.log(chalk.cyan('      _      _        _     _           _     '));
 console.log(chalk.cyan('  __| | ___| | _____| |__ (_)_ __   __| |___ '));
-console.log(chalk.cyan(' / _` |/ _ \ |/ / _ \ |_ \| | \ \ / /| / __|'));
-console.log(chalk.cyan('| (_| |  __/   <  __/ |_) | | |\ V / | \__ \\'));
-console.log(chalk.cyan(' \__,_|\___|_|\_\___|_.__/|_|_| \_/  |_|___/'));
+console.log(chalk.cyan(' / _` |/ _ \\ |/ / _ \\ |_ \\| | \\ \\ / /| / __|'));
+console.log(chalk.cyan('| (_| |  __/   <  __/ |_) | | |\\ V / | \\__ \\'));
+console.log(chalk.cyan(' \\__,_|\\___|_|\\_\\___|_./|_|_| \\_/  |_|___/'));
 console.log();
 
 console.log(`${chalk.bold('User:')}     ${os.userInfo().username}`);
@@ -52,3 +122,18 @@ console.log(`${chalk.bold('Packages:')} ${getPackages()}`);
 console.log(`${chalk.bold('NodeJS:')}   ${process.version}`);
 console.log(`${chalk.bold('CPU:')}      ${os.cpus()[0].model}`);
 console.log(`${chalk.bold('Memory:')}   ${(os.totalmem() / (1024 ** 3)).toFixed(2)} GB`);
+
+// Print all local IPs like: Local IP (eth0): 192.168.8.178/24
+const localIPs = getLocalIPs();
+if (localIPs.length > 0) {
+  for (const ip of localIPs) {
+    console.log(`${chalk.bold(`Local IP (${ip.name}):`)} ${ip.address}/${ip.maskBits}`);
+  }
+} else {
+  console.log(`${chalk.bold('Local IP:')} N/A`);
+}
+
+console.log(`${chalk.bold('Battery:')}  ${getBattery()}`);
+console.log(`${chalk.bold('Locale:')}   ${getLocale()}`);
+
+printColorBars();
